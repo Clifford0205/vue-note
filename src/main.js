ed583306fe64173e1b81bfa0f7e3dcf80aa806e0
田仲展 <ts02690140@gmail.com>
2020-05-25 23:50:26 +0800

    feat(select): select v-model

--- src/main.js
+++ src/main.js
@@ -2,8 +2,10 @@ import Vue from 'vue';
 import App from './App.vue';
 import router from './router';
 import store from './store';
+import Select2 from 'v-select2-component';
 
 Vue.config.productionTip = false;
+Vue.component('Select2', Select2);
 
 new Vue({
   router,
---
+++ src/views/Select.vue
@@ -0,0 +1,46 @@
+<template>
+  <div>
+    <select name="" id="">
+      <option v-for="(item, index) in selectData" :value="item.id" :key="index">
+        {{ item }}
+      </option>
+    </select>
+  </div>
+</template>
+
+<script>
+import ChildrenComponent from '@/components/sync/ChildrenComponent';
+export default {
+  components: {
+    ChildrenComponent
+  },
+  data() {
+    return {
+      selectData: [
+        {
+          id: 1,
+          text: '狗'
+        },
+        {
+          id: 2,
+          text: '豬'
+        },
+        {
+          id: 3,
+          text: '雞'
+        }
+      ]
+    };
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+ul {
+  li {
+    text-align: left;
+    margin-bottom: 10px;
+    color: red;
+  }
+}
+</style>
